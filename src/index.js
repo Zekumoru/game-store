070a6c2147a01fc694e2a76c5a14869f4227a7c3
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-09 20:37:50 +0100

    Import normalize first and remove headings margin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import 'normalize.css';
 import './styles/custom-fonts.css';
 import './styles/utility-classes.css';
 import './index.css';
 import App from './components/App';
-import 'normalize.css';
 import './registerSwiper';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
