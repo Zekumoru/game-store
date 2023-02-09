c8227de3850d0e147afdb27e9c53fdf3f2d4ec60
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-09 15:13:09 +0100

    Fetch also screenshots

--- src/utils/fetchGame.js
+++ src/utils/fetchGame.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 import fetchPrices from './fetchPrices';
 
+const fetchScreenshots = async (id) => {
+  const response = await axios.get(
+    `https://api.rawg.io/api/games/${id}/screenshots?key=f8c4731c17aa4d39a151c2de730a4e53`
+  );
+  return response.data.results;
+};
+
 const fetchGame = async (id) => {
   const response = await axios.get(
     `https://api.rawg.io/api/games/${id}?key=f8c4731c17aa4d39a151c2de730a4e53`
   );
   const game = response.data;
+  game.screenshots = await fetchScreenshots(id);
   return (await fetchPrices([game]))[0];
 };
 
