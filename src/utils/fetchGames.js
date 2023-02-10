31ffb97fc62dda7b6b5153f82f38fd81709b0a39
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-10 04:29:59 +0100

    Handle error on fetchGames

--- src/utils/fetchGames.js
+++ src/utils/fetchGames.js
@@ -6,9 +6,18 @@ const fetchGames = async (
   { limit = Infinity, setNextUrlCallback = () => {} }
 ) => {
   const response = await axios.get(url);
-  const games = await fetchPrices(response.data.results.slice(0, limit));
-  setNextUrlCallback(response.data.next);
-  return games;
+
+  try {
+    const games = await fetchPrices(response.data.results.slice(0, limit));
+    setNextUrlCallback(response.data.next);
+    return games;
+  } catch (e) {
+    console.error(
+      `An error occurred while fetching games! Make sure that the url and/or query parameters are correct! It is also possible that the data has not been found.\nUrl: ${url}`
+    );
+    console.error('Error:', e.message);
+    return [];
+  }
 };
 
 export default fetchGames;
