6b59f31853c9d4b95f7e04c74b73c7576ca4ac81
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-10 17:01:22 +0100

    Add once option to disable asyncOnce if needed

--- src/components/hooks/useGames.js
+++ src/components/hooks/useGames.js
@@ -3,34 +3,47 @@ import fetchGames from '../../utils/fetchGames';
 import useAsyncOnce from './useAsyncOnce';
 import useSessionStorage from './useSessionStorage';
 
-function useGames({ key, url, limit = Infinity, shouldFetch = true }) {
+function useGames({
+  key,
+  url,
+  limit = Infinity,
+  shouldFetch = true,
+  once = true,
+}) {
   const [asyncOnce] = useAsyncOnce();
   const [games, setGames] = useSessionStorage(key, []);
-  const [nextUrl, setNextUrl] = useSessionStorage(`${key}-next-url`, []);
+  const [nextUrl, setNextUrl] = useSessionStorage(`${key}-next-url`, '');
 
   useEffect(() => {
     if (games.length !== 0) return;
     if (!shouldFetch) return;
 
-    asyncOnce(async () => {
+    const fetch = async () => {
       const games = await fetchGames(url, {
         setNextUrlCallback: setNextUrl,
         limit,
       });
       setGames(games);
-    });
+    };
+
+    if (once) asyncOnce(fetch);
+    else fetch();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleLoadMore = () => {
     if (!shouldFetch) return;
+    if (nextUrl === '') return;
 
-    asyncOnce(async () => {
+    const fetch = async () => {
       const newGames = await fetchGames(nextUrl, {
         setNextUrlCallback: setNextUrl,
       });
       setGames((games) => [...games, ...newGames]);
-    });
+    };
+
+    if (once) asyncOnce(fetch);
+    else fetch();
   };
 
   return { games, handleLoadMore };
