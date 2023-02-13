dac6174ce29d5d3afda5f9dba676c6480be08f3e
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-13 02:53:26 +0100

    Create CartContext

---
+++ src/components/contexts/CartItemsContext.js
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from 'react';
+import { useCart } from './CartContext';
+
+const CartItemsContext = React.createContext();
+
+function useCartItems() {
+  return useContext(CartItemsContext);
+}
+
+function CartItemsProvider({ children }) {
+  const [items, setItems] = useState([]);
+  const cart = useCart();
+
+  cart.addToCart = (item) => {
+    setItems((items) => [...items, item]);
+  };
+
+  cart.removeFromCart = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+
+  cart.inTheCart = (id) => {
+    return items.findIndex((item) => item.id === id) !== -1;
+  };
+
+  return (
+    <CartItemsContext.Provider value={items}>
+      {children}
+    </CartItemsContext.Provider>
+  );
+}
+
+export { CartItemsProvider, useCartItems };
