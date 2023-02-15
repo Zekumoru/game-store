63143ef7c3044e9c1d1b657077659e7a7781edc9
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-15 03:33:13 +0100

    Style Cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,38 +3,38 @@ import getPriceText from '../utils/getPriceText';
 import CartList from './cart-list/CartList';
 import { useCartItems } from './contexts/CartItemsContext';
 import useCurrency from './hooks/useCurrency';
+import './styles/Cart.scss';
 
 function Cart() {
   const items = useCartItems();
   const currency = useCurrency();
 
   return (
-    <div>
-      <div className="container">
-        <h1 className="mg-b4">Cart</h1>
-        <div>
-          {items.length} item{items.length !== 1 ? 's' : ''}
-        </div>
-        <div className="horizontal-divider" />
-        <CartList items={items} />
-        <div className="horizontal-divider" />
-        <div className="container flex-space-between">
-          <div>Subtotal</div>
-          <div>
-            {getPriceText({
-              text: currency.sample_text,
-              value: items.reduce(
-                (subtotal, item) => subtotal + item.price.value,
-                0
-              ),
-              currency: {
-                symbol: currency.symbol,
-                placement: currency.placement,
-              },
-            })}
-          </div>
+    <div className="Cart container">
+      <h1 className="mg-b4">Cart</h1>
+      <div>
+        {items.length} item{items.length !== 1 ? 's' : ''}
+      </div>
+      <div className="horizontal-divider" />
+      <CartList items={items} />
+      <div className="horizontal-divider" />
+      <div className="subtotal | flex-space-between">
+        <div>Subtotal</div>
+        <div className="price">
+          {getPriceText({
+            text: currency.sample_text,
+            value: items.reduce(
+              (subtotal, item) => subtotal + item.price.value,
+              0
+            ),
+            currency: {
+              symbol: currency.symbol,
+              placement: currency.placement,
+            },
+          })}
         </div>
       </div>
+      <button className="button fw-bold">Checkout</button>
     </div>
   );
 }
