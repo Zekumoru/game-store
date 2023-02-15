41d006de80e4692c0116b7f7cbc8c3934e15cdce
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-15 16:42:25 +0100

    Add subitems to sidebar nav item

--- src/components/sidebar-navigation/SidebarNavItem.js
+++ src/components/sidebar-navigation/SidebarNavItem.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Icon from '../../assets/icons';
 import './styles/SidebarNavItem.scss';
 
-function SidebarNavItem({ children, icon }) {
+function SidebarNavItem({ children, subitems, icon }) {
   return (
     <div className="SidebarNavItem">
-      <Icon className="icon" icon={icon} />
-      <div className="label fw-bold">{children}</div>
+      <div className="title">
+        <Icon className="icon" icon={icon} />
+        <div className="label fw-bold">{children}</div>
+      </div>
+      {subitems && (
+        <ul>
+          {subitems.map((subitem) => (
+            <li key={subitem.id}>{subitem.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
--- src/components/sidebar-navigation/SidebarNavigation.js
+++ src/components/sidebar-navigation/SidebarNavigation.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { cartIcon, homeIcon, pcIcon, swordIcon } from '../../assets/icons';
 import SidebarNavItem from './SidebarNavItem';
 import './styles/SidebarNavigation.scss';
+import platforms from '../../data/platforms.json';
+import genres from '../../data/genres.json';
 
 function SidebarNavigation() {
   return (
     <div className="SidebarNavigation">
       <SidebarNavItem icon={homeIcon}>Home</SidebarNavItem>
       <SidebarNavItem icon={cartIcon}>Cart</SidebarNavItem>
-      <SidebarNavItem icon={pcIcon}>Platforms</SidebarNavItem>
-      <SidebarNavItem icon={swordIcon}>Genres</SidebarNavItem>
+      <SidebarNavItem subitems={platforms} icon={pcIcon}>
+        Platforms
+      </SidebarNavItem>
+      <SidebarNavItem subitems={genres} icon={swordIcon}>
+        Genres
+      </SidebarNavItem>
     </div>
   );
 }
