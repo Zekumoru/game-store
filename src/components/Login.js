0182593c884dd7cff6e40a08d4561f3b00b948ce
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-16 04:16:07 +0100

    Fix form image bg not filling up whole screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,25 +7,27 @@ import './styles/form-page.scss';
 function SignUp() {
   return (
     <div className="form-page container full-screen">
-      <LoginSignUpForm title="Login">
-        <LabelInput
-          label="Email"
-          type="email"
-          className="text-input"
-          requiredMessage="Email is required"
-        />
-        <LabelInput
-          label="Password"
-          type="password"
-          className="password-input"
-          requiredMessage="Password is required"
-          minLength="8"
-          maxLength="20"
-        />
-        <button className="button fw-bold">Login</button>
-      </LoginSignUpForm>
-      <div className="user-prompt-text">
-        New user? <Link to="/signup">Create an account</Link>
+      <div className="form-container">
+        <LoginSignUpForm title="Login">
+          <LabelInput
+            label="Email"
+            type="email"
+            className="text-input"
+            requiredMessage="Email is required"
+          />
+          <LabelInput
+            label="Password"
+            type="password"
+            className="password-input"
+            requiredMessage="Password is required"
+            minLength="8"
+            maxLength="20"
+          />
+          <button className="button fw-bold">Login</button>
+        </LoginSignUpForm>
+        <div className="user-prompt-text">
+          New user? <Link to="/signup">Create an account</Link>
+        </div>
       </div>
     </div>
   );
