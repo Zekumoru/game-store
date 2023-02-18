a1b208751dd290cdf31a08b1ff212a85ddf2aebd
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-18 12:57:13 +0100

    Change to https

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 import App from './components/App';
 import './registerSwiper';
 
-axios.defaults.baseURL = 'http://194.163.190.50:3010/game-store-api';
+axios.defaults.baseURL = 'https://194.163.190.50:3010/game-store-api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
